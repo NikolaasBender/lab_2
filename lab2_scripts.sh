506666ead68ef7f5ec89c762a2e6fb0a1eb23dc2
Nick <superpineappleperson@gmail.com>
2019-01-31 17:56:39 -0700

    hey prof, can i move on yet?

--- lab2_scripts.sh
+++ lab2_scripts.sh
@@ -2,13 +2,26 @@
 # Authors : Your Name & Your partner's name
 # Date: 1/1/2019
 
-#Problem 1 Code:
-#Make sure to document how you are solving each problem!
+findfile='regex_practice.txt'
+phonetxt='phone_results.txt'
+emailtxt='email_results.txt'
 
-phoneregex = /^[\.-)( ]*([0-9]{3})[\.-)( ]*([0-9]{3})[\.-)( ]*([0-9]{4})$/
-findfile = regex_practice.txt
-phonetxt = phone_results.txt
-emailtxt = email_results.txt
+#1 COUNT PHONE NUMBERS
+grep '\(([0-9]\{3\})\|[0-9]\{3\}\)[ -.]\?[0-9]\{3\}[ -.]\?[0-9]\{4\}' $findfile -c
 
-#COUNT PHONE NUMBERS
-grep '/^[\.-)( ]*([0-9]{3})[\.-)( ]*([0-9]{3})[\.-)( ]*([0-9]{4})$/' $findfile -c >> $outfile
+#2 COUNT THE NUMBER OF EMAILS 
+grep -E -o "\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,6}\b" $findfile -c
+
+#3 LIST ALL 303 PHONE NUMBERS
+grep '\((303)\|303\)[ -.]\?[0-9]\{3\}[ -.]\?[0-9]\{4\}' $findfile >> $phonetxt
+
+#4 LIST ALL @geocities.com EMAILS
+grep '@geocities.com' $findfile >> $emailtxt
+
+read -p "Enter some search string: " searcher
+echo
+grep $searcher $findfile >> 'comand_results.txt'
+
+read -p "Enter anything to reset: " searcher
+echo
+rm $phonetxt $emailtxt
